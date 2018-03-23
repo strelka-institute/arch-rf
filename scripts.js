53ec4b81fac16dd75118beca82631c2129859ba9
razdvapoka <razdvapoka@gmail.com>
2018-03-23 15:35:25 +0300

    :sparkles: Add all the other rooms and CNAME file

--- scripts.js
+++ scripts.js
@@ -11,15 +11,14 @@ document.addEventListener('DOMContentLoaded', event => {
 
 
 const showHint = () => {
-  const hint = document.createElement('div')
-  hint.classList.add('hint')
-  document.querySelector('body').appendChild(hint)
+  const hint = document.querySelector('.hint')
+  hint.classList.add('hint-visible')
   isShowingHint = true
 }
 
 const hideHint = () => {
   const hint = document.querySelector('.hint')
-  document.querySelector('body').removeChild(hint)
+  hint.classList.remove('hint-visible')
   isShowingHint = false
 }
 
@@ -31,25 +30,31 @@ const handleSubmit = (e) => {
   e.preventDefault()
   const form = $('#mc-embedded-subscribe-form')
   const data = form.serialize()
-  const email = $('#mce-EMAIL').value
-  const inputs = $('#mc-embedded-subscribe-form input')
-  inputs.attr('disabled', true)
-  $.ajax({
-      type        : 'GET',
-      url         : MAILCHIMP_URL,
-      data        : data,
-      cache       : false,
-      dataType    : 'json',
-      contentType: "application/json; charset=utf-8",
-      error       : function(err) { console.log('ERR:', err) },
-      success     : function(data) {
-        inputs.attr('disabled', false)
-        if (data.result !== "success") {
-
-        } else {
+  const email = $('.email')[0].value
+  if (validateEmail(email)) {
+    const inputs = $('#mc-embedded-subscribe-form input')
+    console.log('SEND!')
+    inputs.attr('disabled', true)
+    $.ajax({
+        type        : 'GET',
+        url         : MAILCHIMP_URL,
+        data        : data,
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { console.log('ERR:', err) },
+        success     : function(data) {
+          inputs.attr('disabled', false)
+          if (data.result === "success") {
+            console.log(data)
+            console.log(Cookies)
+            Cookies.set(ALREADY_SUBSCRIBED_COOKIE, 'true')
+          } else {
+            console.log(data)
+          }
         }
-      }
-  });
+    });
+  }
 }
 
 const initScrollMagic = () => {
@@ -57,17 +62,19 @@ const initScrollMagic = () => {
 
   const wipeAnimation = new TimelineMax()
     .to("#plan .room-door-1", 0.5, { rotation: -90, ease: Linear.easeNone }, 0)
-    .to("#plan .room-door-2", 0.5, { rotation: -90, ease: Linear.easeNone }, 1)
+    .to("#plan .room-door-2", 0.5, { rotation: -90, x: "-1.5px", ease: Linear.easeNone }, 1)
     .fromTo("#plan .room-1", 2, { y: "0%" }, { y: "-66%", ease: Linear.easeNone }, 0.5)
     .to("#plan .room-1", 1, { x: "-100%", ease: Linear.easeNone }, 2.5)
-    .fromTo("#plan .room-3", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone }, 2.5)
-    .to("#plan .room-3", 1, { y: "-100%", ease: Linear.easeNone }, 3.5)
-    .fromTo("#plan .room-4", 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone }, 3.5)
+    .fromTo("#plan .room-door-3", 0.5, { rotation: 90 },  { rotation: 0, x: "-1.5px", ease: Linear.easeNone }, 3)
+    .to("#plan .room-1", 1, { y: "-100%", ease: Linear.easeNone }, 3.5)
+    .fromTo("#plan .room-5", 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone }, 3.5)
+    .to("#plan .room-5", 1, { y: "-100%", ease: Linear.easeNone }, 4.5)
+    .fromTo("#plan .room-6", 1, { y: "100%" }, { y: "0%", ease: Linear.easeNone }, 4.5)
 
   const scene = new ScrollMagic.Scene({
       triggerElement: "#plan",
       triggerHook: "onLeave",
-      duration: "350%"
+      duration: "400%"
     })
     .setPin("#plan")
     .setTween(wipeAnimation)
@@ -78,7 +85,7 @@ const initScrollMagic = () => {
 
 const initHinting = (wipeAnimation) => {
   setInterval(() => {
-    if (downtime > 5000 && !isShowingHint && wipeAnimation.progress() < 1) {
+    if (downtime > 3000 && !isShowingHint && wipeAnimation.progress() < 1) {
       showHint()
       downtime = 0
     } else {
@@ -94,13 +101,33 @@ const initHinting = (wipeAnimation) => {
   })
 }
 
+const handleEmailChange = (e) => {
+  console.log(e)
+  const value = e.target.value
+  const isValid = validateEmail(value)
+  console.log(value, isValid)
+  if (isValid) {
+    document.querySelector('.button').classList.add('active-button')
+  } else {
+    document.querySelector('.button').classList.remove('active-button')
+  }
+}
+
 const initForm = () => {
+  const isAlreadySubscribed = Cookies.get(ALREADY_SUBSCRIBED_COOKIE)
+  console.log(isAlreadySubscribed)
+
   const subscribeForm = document.querySelector('#mc-embedded-subscribe-form')
   subscribeForm.addEventListener('submit', handleSubmit)
+
+  const emailInput = document.querySelector('.email')
+  emailInput.addEventListener('input', handleEmailChange)
 }
+
 
 const init = () => {
   const wipeAnimation = initScrollMagic()
   initHinting(wipeAnimation)
   initForm()
+  showHint()
 }
